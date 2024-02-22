48640d777bb2e8bca647e2bc843e32feabc1cefb
edmocosta <11836452+edmocosta@users.noreply.github.com>
2024-02-22 18:21:10 +0100

    Fix comment typo

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -3,6 +3,6 @@
 set -ex
 
 # Avoid gem naming conflicts as the logstash-input-snmp and logstash-input-snmptrap plugins
-# are embedded is a few Logstash versions.
+# are embedded with a few Logstash versions.
 nohup /usr/share/logstash/bin/logstash-plugin remove logstash-input-snmp
 nohup /usr/share/logstash/bin/logstash-plugin remove logstash-input-snmptrap
