f68e02fdd0d5f9f1d580e6724171b0cf11f95ce3
João Duarte <jsvduarte@gmail.com>
2024-11-19 10:37:00 +0000

    remove the removal of individual plugins

--- .ci/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-# Avoid gem naming conflicts as the logstash-input-snmp and logstash-input-snmptrap plugins
-# are embedded with a few Logstash versions.
-#nohup /usr/share/logstash/bin/logstash-plugin remove logstash-input-snmp
-#nohup /usr/share/logstash/bin/logstash-plugin remove logstash-input-snmptrap
